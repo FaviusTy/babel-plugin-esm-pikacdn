e6b8486b53c6c7d1ed6ada34da9399b9060dea36
Favi_ty <favius983@gmail.com>
2020-02-22 00:01:31 +0900

    wip: added test patterns

---
+++ test/fixtures/export-pattern/code.js
@@ -0,0 +1,5 @@
+export { default as module6, sub3 } from "reframe";
+export * from "mitt";
+export const a = "a";
+export function b() {}
+export default "";
---
+++ test/fixtures/export-pattern/output.js
@@ -0,0 +1,8 @@
+export {
+  default as module6,
+  sub3
+} from "https://cdn.pika.dev/reframe.js/2.2.7";
+export * from "https://cdn.pika.dev/mitt/1.2.0";
+export const a = "a";
+export function b() {}
+export default "";
